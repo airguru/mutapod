8fde1bcac12fa411a42b5607a2b3bdc87ee20c0f
Pavel Kuriščák <pavel.kuriscak@g-t.cz>
2026-04-10 12:30:43 +0200

    Include CLI entrypoint in releases

---
+++ cmd/mutapod/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/mutapod/mutapod/internal/cli"
+
+	// Register providers
+	_ "github.com/mutapod/mutapod/internal/provider/gcp"
+)
+
+func main() {
+	cli.Execute()
+}
